017a9e9f98de95a182b552a18a30c7d6571ec581
h-sud <h-sud@vanke.com>
2018-06-15 09:00:16 +0800

    dome开发

--- src/views/dome/mutative-router.vue
+++ src/views/dome/mutative-router.vue
@@ -9,13 +9,52 @@
             <div>
                 <Row>
                     <Col span="24">
-                    <Card>
+                    <Card style="height: 450px">
                         <p slot="title">
 
-                            地方科研基础条件和能力12
+                            地方科研基础条件和能力建设
                         </p>
-                        <Input v-model="value" placeholder="Enter something..." style="width: 300px"></Input>
+                        <Form :model="formItem" :label-width="120" inline>
 
+                            <FormItem label="计划类别:">
+                                <Input v-model="formItem.input1" disabled style="width: 220px" ></Input>
+                            </FormItem>
+                            <FormItem label="申报单位:">
+                                <Input v-model="formItem.input2" disabled style="width: 220px" ></Input>
+                            </FormItem>
+                            <FormItem label="项目负责人:">
+                                <Input v-model="formItem.input3" disabled style="width: 220px" ></Input>
+                            </FormItem>
+                            <FormItem label="初审单位类型:">
+                                <Select v-model="formItem.input4"  style="width: 220px">
+                                    <Option value="beijing">政务机构</Option>
+                                    <Option value="shanghai">各科技局</Option>
+                                    <Option value="shenzhen">高等院校</Option>
+                                </Select>
+                            </FormItem>
+                            <FormItem label="初审单位名称:">
+                                <Input v-model="formItem.input5" style="width: 220px" ></Input>
+                            </FormItem>
+                            <FormItem label="初审财政部门名称:">
+                                <Input v-model="formItem.input6" style="width: 220px" ></Input>
+                            </FormItem>
+                            <FormItem label="主管部门:">
+                                <Input v-model="formItem.input7" style="width: 220px" ></Input>
+                            </FormItem>
+                            <FormItem label="申报日期:">
+
+                                <DatePicker type="date" placeholder="Select date" v-model="formItem.input8" style="width: 220px" ></DatePicker>
+
+
+                            </FormItem>
+                            <FormItem label="项目起止日期:">
+                                <Input v-model="formItem.input9" style="width: 220px" ></Input>
+                            </FormItem>
+                            <FormItem >
+                                <Button type="primary"  @click="handleSubmit()">保存</Button>
+                                <Button type="ghost" style="margin-left: 8px">重置</Button>
+                            </FormItem>
+                        </Form>
                     </Card>
                     </Col>
                 </Row>
@@ -29,12 +68,42 @@
 </template>
 
     <script>
-  export default {
-    data () {
-      return {
-        value: ''
-      }
-    }
-  }
+      export default {
+        data () {
+          return {
+            formItem: {
+              input1: '地方科研基础条件和能力建设',
+              input2: '北京志鸿中科科技有限公司',
+              input3: '汪铁伦',
+              select: '',
+              radio: 'male',
+              checkbox: [],
+              switch: true,
+              date: '',
+              time: '',
+              slider: [20, 50],
+              textarea: ''
+            }
+          }
+        },
+        methods: {
+          handleSubmit () {
+            this.$ajax({
+              method: 'post',
+              url: '/api/drummer/8bd17859',
+              data: {
+                code: '1234567',
+                username: 'Joyce'
+              }
+            }).then(response=>{
+              let _data=response.data;
+              alert("hello," + _data.username);
+            }).catch(function(err){
+              this.$Message.error('保存失败');
+            }.bind(this));
+
+          }
+        }
+      };
 </script>
 
